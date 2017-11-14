7d964df27a356e1f8deb7049881ccdcba9e52eec
montrells <montrell.story@gmail.com>
2017-11-14 00:08:26 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
+*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [cards[0]];
+var cardTwo = [cards[3]];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+if (cardsInPlay.length === 2) {
+    console.log("Two cards are present!");
+}if (cardsInPlay[0] === cardsInPlay[3]){
+    alert("You found a match!");
+}else {
+    alert("Sorry, try again.");
+}
+
+console.log(cardsInPlay);
+
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+/*
 console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardFour); */
+
+
