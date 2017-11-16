12ca65ca42292bda96ee48883063393802fef005
montrells <montrell.story@gmail.com>
2017-11-16 13:13:32 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,47 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+    ];
 var cardsInPlay = [];
 var checkForMatch = function () {
 
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+    if (cardsInPlay[0] === cardsInPlay[2]) {
+        alert("You found a match!"); //console to check with terminal, and alert to display in browser
     } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
     }
 }
 
 var flipCard = function (cardId) {
 
-    console.log("User flipped "+ cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped "+ cards[cardId].rank); //accessing the rank specifically out of the array
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage); //accessing the cardImage specifically out of the array
+    cardsInPlay.push(cards[cardId].cardImage);
+    console.log(cards[cardId].suit); //accessing the suit specifically out of the array
+    cardsInPlay.push(cards[cardId].suit);
     checkForMatch()
 }
 
@@ -23,11 +51,5 @@ flipCard(0);
 flipCard(2);
 
 
-
-
-
-/*
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour); */
 
 
